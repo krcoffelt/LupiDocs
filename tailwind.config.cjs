187466920c4655cc5be2219f2463fe18d272a90d
Kyle Coffelt <krcoffelt@gmail.com>
2026-02-25 17:08:24 -0600

    Improve site performance with local Tailwind build and deferred bookshelf loading

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./privacy.html",
+    "./accessibility.html",
+    "./sitemap.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
